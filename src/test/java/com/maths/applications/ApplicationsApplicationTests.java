76f425e12e824436e09cc61110da341ef0b67d24
Matheus Mendes <matheusmendesreis@outlook.com>
2024-11-07 14:41:01 -0300

    first commit

---
+++ src/test/java/com/maths/applications/ApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maths.applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
